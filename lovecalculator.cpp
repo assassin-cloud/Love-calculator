abad9383ba2f84835e33c961374c1f0c5bfa5d52
Assassin Spider <raif75219@gmail.com>
2026-09-13 18:38:14 +0500

    Refactor love calculator with menu and input validation
    Refactored love calculator to include menu options and improved input handling. Updated compatibility calculation logic and added user prompts.

--- lovecalculator.cpp
+++ lovecalculator.cpp
@@ -2,21 +2,39 @@
 #include<string>
 using namespace std;
 
-void welcome(string& name1, string& name2){
-    cout << "=======================" << endl;
+void welcome(int& input){
+    cout << "======================" << endl;
     cout << "   Love Calculator    " << endl;
-    cout << "=======================" << endl;
+    cout << "======================" << endl;
+    cout << "DON'T TAKE THIS SERIOUSLY" << endl;
     cout << endl;
+    cout << "1. Calculate" << endl;
+    cout << "2. Exit" << endl;
+    cout << "Input:" << endl;
+    cin >> input;
+    cin.ignore();
+}
+
+void getuserinput(string& name1, string& name2){
+    cout << endl;
+    cout << "Enter your names to calculate" << endl;
     cout << "Input first name: " << endl;
     getline(cin, name1);
     cout << "Input second name: " << endl;
     getline(cin, name2);
 }
 
+void wait(){
+    cout << "Type anything to continue" << endl;
+    string anything;
+    cin >> anything;
+    cin.ignore();
+}
+
 int calculate(string x, string y){
     int name1size = x.size();
     int name2size = y.size();
-    return (name1size * name2size * 2);
+    return (((name1size * name2size)%100)*2);
 }
 
 void calculating(int a){
@@ -29,10 +47,10 @@ void calculating(int a){
     if(a>=80){
         cout << "You are meant for each other..." << endl;
     }
-    else if(a>=60){
-        cout << "You are in love..." << endl;
+    else if(a>=65){
+        cout << "He/She definitly loves you..." << endl;
     }
-    else if(a>=30){
+    else if(a>=35){
         cout << "Maybe you have a chance..." << endl;
     }
     else{
@@ -40,29 +58,39 @@ void calculating(int a){
     }
 }
 
-void getinput(string& end){
-    cout << "Type y to run the program or anything to quit:" << endl;
-    cin >> end;
-    cin.ignore();
-}
-
 int main(){
-    string end = "y";
+    int input {};
     string firstname {};
     string secondname {};
-    while(end == "y"){
-        welcome(firstname, secondname);
-        int lovecalculate = calculate(firstname, secondname);
-        if(lovecalculate>100){
-            lovecalculate = 100;
-            calculating(lovecalculate);
-            cout << endl;
-            getinput(end);
+    while(true){
+        welcome(input);
+        if(cin.fail()){
+            cout << "Invalid Input!" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
         }
         else{
-            calculating(lovecalculate);
-            cout << endl;
-            getinput(end);
+            if(input == 1){
+                getuserinput(firstname, secondname);
+                cout << endl;
+                double calculatelove = calculate(firstname, secondname);
+                if(calculatelove>100){
+                    calculatelove = 100;
+                    calculating(calculatelove);
+                    cout << endl;
+                    wait();
+                }
+                else{
+                    calculating(calculatelove);
+                    wait();
+                }
+            }
+            else if(input == 2){
+                break;
+            }
+            else {
+                cout << "Invalid Input!" << endl;
+            }
         }
     }
 }
